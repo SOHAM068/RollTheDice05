aa219c777614da7025d09463b899dc68cc8f8204
SOHAM068 <sg3533@srmist.edu.in>
2024-04-11 07:57:39 +0530

    1st one

---
+++ src/App.tsx
@@ -0,0 +1,97 @@
+import { StyleSheet, Text, View, Pressable, Image, ImageSourcePropType } from 'react-native'
+import React from 'react'
+import { useState } from 'react'
+import { PropsWithChildren } from 'react'
+import ReactNativeHapticFeedback from "react-native-haptic-feedback";
+
+import DiceOne from '../assets/One.png'
+import DiceTwo from '../assets/Two.png'
+import DiceThree from '../assets/Three.png'
+import DiceFour from '../assets/Four.png'
+import DiceFive from '../assets/Five.png'
+import DiceSix from '../assets/Six.png'
+
+type DiceProp = PropsWithChildren<{
+  imageUrl : ImageSourcePropType
+}>
+
+const Dice = ({imageUrl} : DiceProp):JSX.Element => {
+  return(
+    <View>
+      <Image source={imageUrl} style={styles.diceImage}/>
+    </View>
+  )
+}
+const options = {
+  enableVibrateFallback: true,
+  ignoreAndroidSystemSettings: false,
+};
+
+export default function App(): JSX.Element {
+  const [diceImage, setDiceImage] = useState<ImageSourcePropType>(DiceOne);
+
+  const rollTheDice = () => {
+    let randomNumber = Math.floor(Math.random() * 6) + 1;
+    switch (randomNumber) {
+      case 1:
+        setDiceImage(DiceOne);
+        break;
+      case 2:
+        setDiceImage(DiceTwo);
+        break;
+      case 3:
+        setDiceImage(DiceThree);
+        break;
+      case 4:
+        setDiceImage(DiceFour);
+        break;
+      case 5:
+        setDiceImage(DiceFive);
+        break;
+      case 6:
+        setDiceImage(DiceSix);
+        break;
+      default:
+        setDiceImage(DiceOne);
+        break;
+    }
+    ReactNativeHapticFeedback.trigger("impactLight", options);
+  };
+  return (
+    <>
+      <View style={styles.container}>
+        <Dice imageUrl={diceImage} />
+        <Pressable onPress={rollTheDice}>
+          <Text style={styles.rollDiceBtnText}>Roll The Dice</Text>
+        </Pressable>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#FFF2F2',
+  },
+  diceContainer: {
+    margin: 12,
+  },
+  diceImage: {
+    width: 200,
+    height: 200,
+  },
+  rollDiceBtnText: {
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    borderWidth: 2,
+    borderRadius: 8,
+    borderColor: '#E5E0FF',
+    fontSize: 16,
+    color: '#8EA7E9',
+    fontWeight: '700',
+    textTransform: 'uppercase',
+  },
+})
